27a04c01723acad6c00be74aa5888272a7d78e0e
Zhou Yangchao <1028519445@qq.com>
2017-03-06 22:45:03 +0800

    alpha release

--- rules/_loader.py
+++ rules/_loader.py
@@ -14,7 +14,7 @@ def __getFieldName(filename):
 
 def __loadRules(filename):
 	rules = []
-	with open(filename) as f:
+	with open(filename, encoding='utf-8', errors='ignore') as f:
 		for line in f.readlines():
 			strs = line.split(' ')
 			if len(strs) > 1:
@@ -23,14 +23,15 @@ def __loadRules(filename):
 
 def load():
 	fieldList = {}
-	for filename in os.listdir(os.path.abspath('.')):
+	dir = os.path.split(os.path.realpath(__file__))[0]
+	for filename in os.listdir(dir):
 		#need skip '.svn'
 		if filename.startswith('.') or filename.startswith('_') or filename.endswith('.pyc'):
 			continue
 		#1.field.rules
 		ret, name = __getFieldName(filename)
 		if ret:
-			fieldList[name] = __loadRules(filename)
+			fieldList[name] = __loadRules(dir + '\\' + filename)
 	return fieldList
 
 if __name__ == '__main__':
