b963a51a25c0b953e03632196cd490f708350c0d
Zhou Yangchao <1028519445@qq.com>
2017-05-19 01:24:02 +0800

    支持按regex方式load

--- rules/_loader.py
+++ rules/_loader.py
@@ -3,6 +3,8 @@
 import os
 import json
 import codecs
+import re
+from pprint import pprint
 
 def __getFieldName(filename):
 	idx = filename.find('.')
@@ -23,6 +25,21 @@ def __loadRules(filename):
 				rules.append(strs)
 	return rules
 
+def __loadRegexes(filename):
+	rules = []
+	with codecs.open(filename, encoding='utf-8', errors='ignore') as f:
+		for line in f.readlines():
+			strs = line.split(' ')
+			if len(strs) > 1:
+				rule = dict()
+				rule['score'] = strs[0]
+				rule['patterns'] = []
+				for str in strs[1:]:
+					p = re.compile(str.rstrip(), re.IGNORECASE)
+					rule['patterns'].append(p)
+				rules.append(rule)
+	return rules
+
 def load_rules():
 	rules = {}
 	dir = os.path.split(os.path.realpath(__file__))[0]
@@ -36,5 +53,20 @@ def load_rules():
 			rules[name] = __loadRules(dir + '\\' + filename)
 	return rules
 
+def load_regexes():
+	rules = {}
+	dir = os.path.split(os.path.realpath(__file__))[0]
+	for filename in os.listdir(dir):
+		#need skip '.svn'
+		if filename.startswith('.') or filename.startswith('_') or filename.endswith('.pyc'):
+			continue
+		#1.field.rules
+		ret, name = __getFieldName(filename)
+		if ret:
+			rules[name] = __loadRegexes(dir + '\\' + filename)
+	return rules
+
 if __name__ == '__main__':
 	print(json.dumps(load_rules(), indent=2, ensure_ascii=False))
+	regexes = load_regexes()
+	pprint(regexes)
