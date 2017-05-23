d3decfc372b965d97d62e6bd71343b39039fcd26
Zhou Yangchao <1028519445@qq.com>
2017-05-23 21:47:37 +0800

    support threading

--- analysis_regex.py
+++ analysis_regex.py
@@ -5,6 +5,7 @@ import codecs
 import json
 import time
 import threading
+import queue
 import sys
 from optparse import OptionParser
 
@@ -26,30 +27,110 @@ def init_parser():
 	return parser.parse_args()
 
 
+class Worker(threading.Thread):
+	def __init__(self):
+		super(Worker, self).__init__()
+		self.stop = False
+		self.in_queue = queue.Queue(1000)
+		self.out_queue = queue.Queue(2000)
+		self.log_queue = queue.Queue(2000)
+		self.statistics = {}
+		pass
+
+	def stop_work(self):
+		self.stop = True
+
+	def get_statistics(self):
+		return self.statistics
+
+	def is_full(self):
+		return self.in_queue.full()
+
+	def enqueue_task(self, line):
+		self.in_queue.put(line)
+
+	def file_output(self, file):
+		while not self.out_queue.empty():
+			out_line = self.out_queue.get()
+			file.write(out_line + '\n')
+		pass
+
+	def file_log(self, file):
+		while not self.log_queue.empty():
+			out_line = self.log_queue.get()
+			file.write(out_line + '\n')
+
+	def run(self):
+		while True:
+			try:
+				line = self.in_queue.get(True, 1)
+				line = self.in_queue.get(True, 1)
+			except:
+				if self.stop:
+					return
+				else:
+					continue
+			try:
+				_json = json.loads(line)
+				c = comment.Comment(_json)
+				match_count = c.match(_json['content'], comment.matchRegex)
+			except:
+				self.log_queue.put(line, True)
+				continue
+
+			try:
+				self.statistics[match_count] += match_count
+			except:
+				self.statistics[match_count] = match_count
+			self.out_queue.put(str(c), True)
+			try:
+				self.statistics['count'] += 1
+			except:
+				self.statistics['count'] = 1
+		pass
+
+
 if __name__ == '__main__':
 	(options, args) = init_parser()
-	match_count = 0
-	count = 0
-	start_time = time.process_time()
 	input = codecs.open(options.input, 'r', encoding='utf-8', errors='strict')
 	output = codecs.open(options.output, 'w', encoding='utf-8')
 	log = codecs.open(options.logfile, 'w', encoding='utf-8')
 	output.write((','.join(comment.OUT_HEADERS) + '\n'))
-	for line in input.readlines():
-		try:
-			_json = json.loads(line)
-			if len(_json['content']) < 10:
-				continue
-			c = comment.Comment(_json)
-			_match_count = c.match(_json['content'], comment.matchRegex)
-		except:
-			log.write(line)
-			continue
 
-		if _match_count:
-			output.write(str(c) + '\n')
-		match_count += _match_count
-		count += 1
-		if count == int(options.max_count):
+	workers = []
+	for i in range(int(options.thread)):
+		workers.append(Worker())
+
+	for worker in workers:
+		worker.start()
+
+	while True:
+		count = 0
+		for worker in workers:
+			# 先将工作队列填满
+			while not worker.is_full():
+				line = input.readline()
+				if line:
+					worker.enqueue_task(line)
+					count += 1
+				else:
+					break
+			# 输出结果
+			worker.file_output(output)
+			# 输出错误日志
+			worker.file_output(log)
+		if count == 0:
 			break
-	print('process {0} count comments, match {1} rules. process {2}s'.format(count, match_count, time.process_time()))
+
+	for worker in workers:
+		worker.stop_work()
+		# 输出结果
+		worker.file_output(output)
+		# 输出错误日志
+		worker.file_output(log)
+
+	for worker in workers:
+		worker.join()
+		print(worker.get_statistics())
+
+	print('process time {0}'.format(time.process_time()))
